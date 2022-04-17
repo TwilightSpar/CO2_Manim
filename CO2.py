f8c5ba8d67df7a1182846a8bf48faeaaf8663a78
Twilight <Twilight@DESKTOP-DISK632>
2022-04-16 20:19:37 -0400

    create

---
+++ CO2.py
@@ -0,0 +1,65 @@
+from manim import *
+
+
+class M1(Scene):
+    def construct(self):
+        M1_Tis_1 = MathTex(r"V_{tis} \derivative{P_{tis} }{t} = \frac {M_{tis} } {k} + Q(P_a^*-P_{tis}) ").shift(UP)
+        Text_consider_nullcline = Tex("let RHS=0, consider stable situation").shift(2*UP)
+        M1_Tis_1_equal_0_1 = MathTex(r"\frac {M_{tis} } {k} + Q(P_a^*-P_{tis}) = 0").shift(UP)
+        M1_Tis_1_equal_0_2 = MathTex(r"\frac {M_{tis} } {k} + QP_a^* = QP_{tis}").shift(UP)
+        M1_Tis_1_equal_0_3 = MathTex(r"QP_{tis} = \frac {M_{tis} } {k} + QP_a^*").shift(UP)
+
+        M_1_mass_conservation_1 = MathTex(r" P_{tis} = (Q_{ot}P_{ot}+Q_mP_m) / Q")
+        M_1_mass_conservation_2 = MathTex(r" QP_{tis} = (Q_{ot}P_{ot}+Q_mP_m)")
+        M_1_mass_conservation_2_plugin_Tis = MathTex(r" \frac {M_{tis}} {k} + QP_a^* = (Q_{ot}P_{ot}+Q_mP_m)")
+
+        # M1_Tis_1.to_edge(UP)
+
+        self.play(Write(M1_Tis_1))
+        self.wait()
+
+        self.play(Write(Text_consider_nullcline))
+        self.wait()
+
+        self.remove(Text_consider_nullcline)
+        self.wait()
+        self.play(M1_Tis_1.animate.shift(UP))
+
+        self.play(Write(M1_Tis_1_equal_0_1))
+        self.wait()
+        self.remove(M1_Tis_1)
+        self.wait()
+        self.play(M1_Tis_1_equal_0_1.animate.shift(UP))
+
+        self.play(Write(M1_Tis_1_equal_0_2))
+        self.wait()
+        self.remove(M1_Tis_1_equal_0_1)
+        self.wait()
+        self.play(M1_Tis_1_equal_0_2.animate.shift(UP))
+        self.wait()
+
+        self.play(Write(M1_Tis_1_equal_0_3))
+        self.wait()
+        self.remove(M1_Tis_1_equal_0_2)
+        self.wait()
+        self.play(M1_Tis_1_equal_0_3.animate.shift(UP*2))
+
+        self.play(Write(M_1_mass_conservation_1))
+        self.wait()
+        self.play(M_1_mass_conservation_1.animate.shift(UP))
+
+        self.play(Write(M_1_mass_conservation_2))
+        self.wait()
+        self.remove(M_1_mass_conservation_1)
+        self.wait()
+        self.play(M_1_mass_conservation_2.animate.shift(UP))
+
+        self.play(Write(M_1_mass_conservation_2_plugin_Tis))
+        self.wait()
+        self.remove(M_1_mass_conservation_2)
+        self.wait()
+        self.remove(M1_Tis_1_equal_0_3)
+        self.wait()
+        self.remove(M_1_mass_conservation_2_plugin_Tis)
+
+        self.wait()
